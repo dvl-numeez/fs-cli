dcef2cf4d6fd31f6c2ab7a6ce902df9d9197346d
numeezbaloch17 <numeez.baloch@digivatelabs.com>
2024-08-23 10:34:38 +0530

    Copy functionality is done

--- copy_files.go
+++ copy_files.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -11,13 +12,41 @@ func Copy(input string){
 	inputs:=strings.Split(input, " ")
 	sourceDestination:=inputs[1]
 	copyDestination:=inputs[2]
+	CopyFiles(sourceDestination,copyDestination)
+}
+
+func CopyFiles(sourceDestination,copyDestination string){
+	fileInfo,err:=os.Stat(sourceDestination)
+	if err!=nil{
+		fmt.Println("Error occured : ",err)	
+	}
+	nameOfFile:=fileInfo.Name()
+	if fileInfo.IsDir(){
+		err:=os.Mkdir(filepath.Join(copyDestination,nameOfFile),os.ModePerm)
+		if err!=nil{
+			fmt.Println("Error occured : ",err)	
+		}
+		files,err:=os.ReadDir(sourceDestination)
+		if err!=nil{
+			fmt.Println("Error occured : ",err)	
+		}
+		for _,file := range files {
+			source:=filepath.Join(sourceDestination,file.Name())
+			copy:=filepath.Join(copyDestination,nameOfFile)
+			CopyFiles(source,copy)
+		}
+
+
+	}else{
 	data,err:= os.ReadFile(sourceDestination)
 	if err!=nil{
 		fmt.Println("Error occured : ",err)
 	}
-	err=os.WriteFile(copyDestination,data,0644)
+	path:=filepath.Join(copyDestination,nameOfFile)
+	err=os.WriteFile(path,data,os.ModePerm)
 	if err!=nil{
 		fmt.Println("Error occured : ",err)
 	}
+}
 
 }
